29df08fb8843c2e0bff69d22beea64578fe9d671
nmowens95 <nmowens95@gmail.com>
2024-02-15 21:15:45 -0600

    Added solution to valid anagram using two hashmaps and comparing each other

---
+++ ValidAnagram.py
@@ -0,0 +1,34 @@
+'''
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+'''
+
+# code
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        s_count, t_count = {}, {}
+        for i, n in enumerate(s):
+            s_count[s[i]] = s_count.get(s[i], 0) + 1
+            t_count[t[i]] = t_count.get(t[i], 0) + 1
+        return s_count == t_count
